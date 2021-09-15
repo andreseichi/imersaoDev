010b7873e6f8a01a49375a1111d55d2b75c4d8a3
André Tashiro <andre_seichi@hotmail.com>
2021-09-15 20:32:17 -0300

    day 3

--- dia-3/scripts/app.js
+++ dia-3/scripts/app.js
@@ -1,34 +1,12 @@
-const secretNumber = Math.round(Math.random() * 11);
-
-const divLimits = document.getElementById('limits');
-const divPlay = document.getElementById('play');
-divPlay.style.display = 'none';
-
-const setLimit = () => {
-  const minValue = document.getElementById('min-value').value;
-  const maxValue = document.getElementById('max-value').value;
-
-  const pDiv = document.querySelector('.guess-text');
-  pDiv.innerHTML = `Digite um número de ${minValue} a ${maxValue}`;
-
-  divPlay.style.display = 'block';
-  divLimits.style.display = 'none';
-
-  return {
-    minValue,
-    maxValue,
-  };
-};
+const secretNumber = Math.floor(Math.random() * 11);
 
 const guessValue = () => {
-  const values = setLimit();
-
   const guess = parseInt(document.getElementById('value').value);
 
   let result = document.getElementById('result');
 
-  guess > values.maxValue || guess < values.minValue
-    ? (result.innerHTML = `Tente um valor entre ${values.minValue} a ${values.maxValue} apenas`)
+  guess > 10 || guess < 0
+    ? (result.innerHTML = `Tente um valor entre 0 a 10 apenas`)
     : guess === secretNumber
     ? (result.innerHTML = `Você acertou 😁 O valor secreto é ${secretNumber}`)
     : (result.innerHTML = `Você errou 😥 ${
